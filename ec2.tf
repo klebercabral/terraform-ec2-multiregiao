801ced71ca884fe420800f7165c2c35587622201
Kleber Cabral <klebercabral@me.com>
2020-01-25 13:10:22 -0300

    vpc_security_group_ids

--- ec2.tf
+++ ec2.tf
@@ -1,59 +1,90 @@
+resource "aws_security_group" "ssh_east" {
+  provider    = "aws"
+  name        = "ssh_web"
+  description = "ssh_web"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "ssh"
+  }
+}
+
 data "aws_ami" "ubuntu_east" {
   most_recent = true
 
   filter {
-    name   = "name"
-    values = ["${var.ec2_east.ami_name_east}"]
+    name      = "name"
+    values    = ["${var.ec2_east.ami_name_east}"]
   }
 
-  owners = ["${var.ec2_east.ami_owners_east}"]
+  owners      = ["${var.ec2_east.ami_owners_east}"]
 }
 
 resource "aws_instance" "web" {
-provider      = "aws"
-  ami           = "${data.aws_ami.ubuntu_east.id}"
-  instance_type = "${var.ec2_east.instance_type_east}"
-  subnet_id     = "${var.ec2_east.subnet_east}"
-  key_name      = "${var.ec2_east.key_name_east}"
-  count         = "${var.ec2_east.count_east}"
-  associate_public_ip_address = true
-  security_groups = ["${var.ec2_east.security_groups_east}"]
+  provider                    = "aws"
+  ami                         = "${data.aws_ami.ubuntu_east.id}"
+  instance_type               = "${var.ec2_east.instance_type_east}"
+  subnet_id                   = "${var.ec2_east.subnet_east}"
+  key_name                    = "${var.ec2_east.key_name_east}"
+  count                       = "${var.ec2_east.count_east}"
+  vpc_security_group_ids      = ["${aws_security_group.ssh_east.id}"]
 
     provisioner "remote-exec" {
       inline = [
         "${var.ec2_east.remote_exec_east}",
       ]
       connection {
-        type     = "ssh"
-        user     = "${var.ec2_east.provisioner_user_east}"
+        type        = "ssh"
+        user        = "${var.ec2_east.provisioner_user_east}"
         private_key = "${file("mac.pem")}"
-        host     = "${self.public_ip}"
+        host        = "${self.public_ip}"
       }
     }
-
   tags = {
     Name = "${var.ec2_east.instance_tag_name_east}"
   }
 }
 
+resource "aws_security_group" "ssh_west" {
+  provider    = "aws.west"
+  name        = "ssh_web"
+  description = "ssh_web"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "ssh"
+  }
+}
+
 resource "aws_instance" "west" {
-  provider      = "aws.west"
-  ami           = "${var.ec2_west.ami_name_west}"
-  instance_type = "${var.ec2_west.instance_type_west}"
-  subnet_id     = "${var.ec2_west.subnet_west}"
-  key_name      = "${var.ec2_west.key_name_west}"
-  associate_public_ip_address = true
-  security_groups = ["${var.ec2_west.security_groups_west}"]
+  provider                    = "aws.west"
+  ami                         = "${var.ec2_west.ami_name_west}"
+  instance_type               = "${var.ec2_west.instance_type_west}"
+  subnet_id                   = "${var.ec2_west.subnet_west}"
+  key_name                    = "${var.ec2_west.key_name_west}"
+  vpc_security_group_ids      = ["${aws_security_group.ssh_west.id}"]
 
     provisioner "remote-exec" {
       inline = [
         "${var.ec2_west.remote_exec_west}",
       ]
       connection {
-        type     = "ssh"
-        user     = "${var.ec2_west.provisioner_user_west}"
+        type        = "ssh"
+        user        = "${var.ec2_west.provisioner_user_west}"
         private_key = "${file("mac_west.pem")}"
-        host     = "${self.public_ip}"
+        host        = "${self.public_ip}"
       }
     }
 
--- variable.tf
+++ variable.tf
@@ -1,27 +1,25 @@
 variable "ec2_east" {
     default = {
-        "ami_name_east" = "IaaSWeek-806d52dafe9b7fddbc4f0d2d41086ed3cfa02a44"
-        "ami_owners_east" = "777015859311"
-        "instance_type_east" = "t2.micro"
-        "subnet_east" = "subnet-094416f4334e66510"
-        "key_name_east" = "mac"
-        "count_east" = 2
-        "security_groups_east" = "sg-01399a06156627e79"
-        "provisioner_user_east" = "ubuntu"
-        "instance_tag_name_east" = "HelloWorld"
-        "remote_exec_east" = "touch /tmp/test"
+        "ami_name_east"             = "IaaSWeek-806d52dafe9b7fddbc4f0d2d41086ed3cfa02a44"
+        "ami_owners_east"           = "777015859311"
+        "instance_type_east"        = "t2.micro"
+        "subnet_east"               = "subnet-76f1d458"
+        "key_name_east"             = "mac"
+        "count_east"                = 2
+        "provisioner_user_east"     = "ubuntu"
+        "instance_tag_name_east"    = "HelloWorld"
+        "remote_exec_east"          = "touch /tmp/test"
     }
 }
 
 variable "ec2_west" {
     default = {
-        "ami_name_west" = "ami-0dd655843c87b6930"
-        "instance_type_west" = "t2.micro"
-        "subnet_west" = "subnet-0e0f3c7cfd074e330"
-        "key_name_west" = "mac_west"
-        "security_groups_west" = "sg-05ce65c7457ab1909"
-        "provisioner_user_west" = "ubuntu"
-        "instance_tag_name_west" = "HelloWorld"
-        "remote_exec_west" = "touch /tmp/test"
+        "ami_name_west"             = "ami-0dd655843c87b6930"
+        "instance_type_west"        = "t2.micro"
+        "subnet_west"               = "subnet-881e10d3"
+        "key_name_west"             = "mac_west"
+        "provisioner_user_west"     = "ubuntu"
+        "instance_tag_name_west"    = "HelloWorld"
+        "remote_exec_west"          = "touch /tmp/test"
     }
 }
