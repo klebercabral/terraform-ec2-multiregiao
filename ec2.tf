9a296c167eeea7a19f49b824930d7296497517a7
Kleber Cabral <klebercabral@me.com>
2020-01-25 07:51:49 -0300

    Map

--- ec2.tf
+++ ec2.tf
@@ -3,36 +3,36 @@ data "aws_ami" "ubuntu_east" {
 
   filter {
     name   = "name"
-    values = [var.ami_name_east]
+    values = [var.ec2.ami_name_east]
   }
 
-  owners = [var.ami_owners_east]
+  owners = [var.ec2.ami_owners_east]
 }
 
 resource "aws_instance" "web" {
   provider = aws
   ami           = data.aws_ami.ubuntu_east.id
-  instance_type = var.instance_type_east
-  subnet_id     = var.subnet_east
-  key_name      = var.key_name_east
-  count         = var.count_east
+  instance_type = var.ec2.instance_type_east
+  subnet_id     = var.ec2.subnet_east
+  key_name      = var.ec2.key_name_east
+  count         = var.ec2.count_east
   associate_public_ip_address = true
-  security_groups = [var.security_groups_east]
+  security_groups = [var.ec2.security_groups_east]
 
     provisioner "remote-exec" {
       inline = [
-        "${var.remote_exec_east}",
+        "${var.ec2.remote_exec_east}",
       ]
       connection {
         type     = "ssh"
-        user     = var.provisioner_user_east
-        private_key = file(var.private_key_east)
+        user     = var.ec2.provisioner_user_east
+        private_key = file(var.ec2.private_key_east)
         host     = self.public_ip
       }
     }
 
   tags = {
-    Name = var.instance_tag_name_east
+    Name = var.ec2.instance_tag_name_east
   }
 }
 
@@ -41,35 +41,35 @@ resource "aws_instance" "web" {
 #
 #  filter {
 #    name   = "name"
-#    values = [var.ami_name_west]
+#    values = [var.ec2.ami_name_west]
 #  }
 #
-#  owners = [var.ami_owners_west]
+#  owners = [var.ec2.ami_owners_west]
 #}
 
 resource "aws_instance" "west" {
   provider = aws.west
-  ami           = var.ami_name_west
-  instance_type = var.instance_type_west
-  subnet_id     = var.subnet_west
-  key_name      = var.key_name_west
-  count         = var.count_west
+  ami           = var.ec2.ami_name_west
+  instance_type = var.ec2.instance_type_west
+  subnet_id     = var.ec2.subnet_west
+  key_name      = var.ec2.key_name_west
+  count         = var.ec2.count_west
   associate_public_ip_address = true
-  security_groups = [var.security_groups_west]
+  security_groups = [var.ec2.security_groups_west]
 
     provisioner "remote-exec" {
       inline = [
-        "${var.remote_exec_west}",
+        "${var.ec2.remote_exec_west}",
       ]
       connection {
         type     = "ssh"
-        user     = var.provisioner_user_west
-        private_key = file(var.private_key_west)
+        user     = var.ec2.provisioner_user_west
+        private_key = file(var.ec2.private_key_west)
         host     = self.public_ip
       }
     }
 
   tags = {
-    Name = var.instance_tag_name_west
+    Name = var.ec2.instance_tag_name_west
   }
 }
---
+++ vars.tf
@@ -0,0 +1,26 @@
+variable "ec2" {
+    type = map
+    default = {
+        "ami_name_east" = "IaaSWeek-806d52dafe9b7fddbc4f0d2d41086ed3cfa02a44"
+        "ami_owners_east" = "777015859311"
+        "instance_type_east" = "t2.micro"
+        "subnet_east" = "subnet-094416f4334e66510"
+        "key_name_east" = "mac"
+        "count_east" = 2
+        "security_groups_east" = "sg-01399a06156627e79"
+        "provisioner_user_east" = "ubuntu"
+        "private_key_east" = "mac.pem"
+        "instance_tag_name_east" = "HelloWorld"
+        "remote_exec_west" = "touch /tmp/test"
+        "ami_name_west" = "ami-0dd655843c87b6930"
+        "instance_type_west" = "t2.micro"
+        "subnet_west" = "subnet-0e0f3c7cfd074e330"
+        "key_name_west" = "mac_west"
+        "count_west" = 1
+        "security_groups_west" = "sg-05ce65c7457ab1909"
+        "provisioner_user_west" = "ubuntu"
+        "private_key_west" = "mac_west.pem"
+        "instance_tag_name_west" = "HelloWorld"
+        "remote_exec_east" = "touch /tmp/test"
+    }
+}
