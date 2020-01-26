04286f8e7859bb1a50a3ceb2f85816870b06be83
Kleber Cabral <klebercabral@me.com>
2020-01-26 10:54:11 -0300

    Current Workspace Interpolation

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region  = "us-east-1"
+  region  = "${terraform.workspace == "production" ? "us-east-1" : "us-east-2"}"
   version = "~> 2.0"
 }
 
--- terrafile.tf
+++ terrafile.tf
@@ -3,9 +3,9 @@ module "servers_east" {
     providers = {
       aws = "aws"
     }
-    ami_name = "ami-04b9e92b5572fa0d1"
+    ami_name = "${terraform.workspace == "production" ? "ami-04b9e92b5572fa0d1" : "ami-0d5d9d301c853a04a"}"
     instance_type = "t2.micro"
-    subnet = "subnet-76f1d458"
+    subnet = "${terraform.workspace == "production" ? "subnet-76f1d458" : "subnet-f3b1ba9b"}"
     key_name = "lab"
     key_path = "lab.pem"
     provisioner_user = "ubuntu"
@@ -13,25 +13,25 @@ module "servers_east" {
     remote_exec = "touch /tmp/test "
 }
 
-module "servers_west" {
-    source = "./servers"
-    providers = {
-      aws = "aws.west"
-    }
-    ami_name = "ami-0dd655843c87b6930"
-    instance_type = "t2.micro"
-    subnet = "subnet-881e10d3"
-    key_name = "lab"
-    key_path = "lab.pem"
-    provisioner_user = "ubuntu"
-    instance_tag_name = "HelloWorld"
-    remote_exec = "touch /tmp/test "
-}
+#module "servers_west" {
+#    source = "./servers"
+#    providers = {
+#      aws = "aws.west"
+#    }
+#    ami_name = "ami-0dd655843c87b6930"
+#    instance_type = "t2.micro"
+#    subnet = "subnet-881e10d3"
+#    key_name = "lab"
+#    key_path = "lab.pem"
+#    provisioner_user = "ubuntu"
+#    instance_tag_name = "HelloWorld"
+#    remote_exec = "touch /tmp/test "
+#}
 
 output "ip_address_east" {
   value = "${module.servers_east.ip_address}"
 }
 
-output "ip_address_west" {
-  value = "${module.servers_west.ip_address}"
-}
+#output "ip_address_west" {
+#  value = "${module.servers_west.ip_address}"
+#}
